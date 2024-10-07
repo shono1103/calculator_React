2fd605c62da27095d8ba5808283e436276fc2435
shono131103 <shono131103@gmail.com>
2024-10-07 23:47:58 +0900

    #2 [fix] 数字、演算、イコール、C、CAごとに関数を割り当てた

--- client/src/calculator.jsx
+++ client/src/calculator.jsx
@@ -8,27 +8,18 @@ import { useEffect } from "react";
 const Calculator = () => {
     const [receivedData, setReceivedData] = useState("");
     const [currentSymbol, setCurrentSymbol] = useState("");
-    const [result, setResult] = useState("");
+    const [result, setResult] = useState(0);
     const [isFirst, setIsFirst] = useState(true);
-    const [isClear, setIsClear] = useState(false);
-    const [isClearAll, setIsClearAll] = useState(false);
+    const [display, setDisplay] = useState(0);
 
-    const inputNumber = (value) => {
-        if (isClear) {
-            setReceivedData(value);
-            setIsClear(false);
-        }
-        else if (isClearAll) {
-            setIsFirst(true);
-            setReceivedData(value);
-            setIsClearAll(false);
-        }
-        else {
-            setReceivedData(receivedData + value);
-        }
+    // 数字ボタンが押された時の処理
+    const pushNumber = (value) => {
+        setReceivedData(receivedData + value);
+        setDisplay(receivedData + value);
     }
 
-    const inputSymbol = (symbol) => {
+    // ＋ー×÷ボタンが押された時の処理
+    const pushSymbol = (symbol) => {
         const num = parseFloat(receivedData);
         if (isFirst) {
             setResult(num);
@@ -37,30 +28,86 @@ const Calculator = () => {
         else {
             if (currentSymbol === "+") {
                 setResult(result + num);
+                setDisplay(result + num);
             }
             else if (currentSymbol === "-") {
                 setResult(result - num);
+                setDisplay(result - num);
             }
             else if (currentSymbol === "*") {
                 setResult(result * num);
+                setDisplay(result * num);
             }
             else if (currentSymbol === "/") {
                 setResult(result / num);
+                setDisplay(result / num);
             }
         }
         setCurrentSymbol(symbol);
-        if (symbol === "=") {
-            setReceivedData(result);
-            setIsFirst(true);
-        }
-        setIsClear(true);
+        Clear();
     }
+
+
+    // イコールボタンが押された時の処理
+    const pushEqual = (non) => {
+        const num = parseFloat(receivedData);
+        if (currentSymbol === "+") {
+            setResult(result + num);
+            setDisplay(result + num);
+        }
+        else if (currentSymbol === "-") {
+            setResult(result - num);
+            setDisplay(result - num);
+        }
+        else if (currentSymbol === "*") {
+            setResult(result * num);
+            setDisplay(result * num);
+        }
+        else if (currentSymbol === "/") {
+            setResult(result / num);
+            setDisplay(result / num);
+        }
+        setIsFirst(true);
+        ClearAll();
+    }
+
+    // Cボタンが押された時の処理
+    const Clear = () => {
+        setReceivedData("");
+    }
+
+    // CAボタンが押された時の処理
+    const ClearAll = () => {
+        setReceivedData("");
+        setCurrentSymbol("");
+        setResult(0);
+        setIsFirst(true);
+    }
+
+    const pushClear = (non) => {
+        Clear();
+        setDisplay(0);
+    }
+
+    const pushClearAll = (non) => {
+        ClearAll();
+        setDisplay(0);
+    }
+
+
 
 
     return (
         <div className="calculator">
-            <Display data={receivedData} symbol={currentSymbol} />
-            <Keyboard inputNum={inputNumber} inputSymbol={inputSymbol} />
+            <Display
+                data={display}
+                symbol={currentSymbol} />
+            <Keyboard
+                pushNum={pushNumber}
+                pushSymbol={pushSymbol}
+                pushEqual={pushEqual}
+                pushClear={pushClear}
+                pushClearAll={pushClearAll} />
         </div>
     );
 }
--- client/src/component/keyboard.jsx
+++ client/src/component/keyboard.jsx
@@ -1,27 +1,27 @@
 import Button from "./keyboradComponent/button";
 import "./keyboard.css";
 
-const Keyboard = ({ inputNum, inputSymbol }) => {
+const Keyboard = ({ pushNum, pushSymbol, pushEqual, pushClear, pushClearAll }) => {
     return (
         <div className="keyboard">
-            <Button input={inputSymbol} name={"CA"} text={"CA"} value={null}></Button>
-            <Button input={inputSymbol} name={"C"} text={"C"} value={null}></Button>
-            <Button input={inputNum} name={"seven"} text={"7"} value={"7"}></Button>
-            <Button input={inputNum} name={"eight"} text={"8"} value={"8"}></Button>
-            <Button input={inputNum} name={"nine"} text={"9"} value={"9"}></Button>
-            <Button input={inputSymbol} name={"div"} text={"÷"} value={"/"}></Button>
-            <Button input={inputNum} name={"four"} text={"4"} value={"4"}></Button>
-            <Button input={inputNum} name={"five"} text={"5"} value={"5"}></Button>
-            <Button input={inputNum} name={"six"} text={"6"} value={"6"}></Button>
-            <Button input={inputSymbol} name={"mul"} text={"×"} value={"*"}></Button>
-            <Button input={inputNum} name={"one"} text={"1"} value={"1"}></Button>
-            <Button input={inputNum} name={"two"} text={"2"} value={"2"}></Button>
-            <Button input={inputNum} name={"three"} text={"3"} value={"3"}></Button>
-            <Button input={inputSymbol} name={"sub"} text={"-"} value={"-"}></Button>
-            <Button input={inputNum} name={"zero"} text={"0"} value={"0"}></Button>
-            <Button input={inputNum} name={"point"} text={"."} value={"."}></Button>
-            <Button input={inputSymbol} name={"equal"} text={"="} value={"="}></Button>
-            <Button input={inputSymbol} name={"add"} text={"+"} value={"+"}></Button>
+            <Button input={pushClearAll} name={"CA"} text={"CA"} value={null}></Button>
+            <Button input={pushClear} name={"C"} text={"C"} value={null}></Button>
+            <Button input={pushNum} name={"seven"} text={"7"} value={"7"}></Button>
+            <Button input={pushNum} name={"eight"} text={"8"} value={"8"}></Button>
+            <Button input={pushNum} name={"nine"} text={"9"} value={"9"}></Button>
+            <Button input={pushSymbol} name={"div"} text={"÷"} value={"/"}></Button>
+            <Button input={pushNum} name={"four"} text={"4"} value={"4"}></Button>
+            <Button input={pushNum} name={"five"} text={"5"} value={"5"}></Button>
+            <Button input={pushNum} name={"six"} text={"6"} value={"6"}></Button>
+            <Button input={pushSymbol} name={"mul"} text={"×"} value={"*"}></Button>
+            <Button input={pushNum} name={"one"} text={"1"} value={"1"}></Button>
+            <Button input={pushNum} name={"two"} text={"2"} value={"2"}></Button>
+            <Button input={pushNum} name={"three"} text={"3"} value={"3"}></Button>
+            <Button input={pushSymbol} name={"sub"} text={"-"} value={"-"}></Button>
+            <Button input={pushNum} name={"zero"} text={"0"} value={"0"}></Button>
+            <Button input={pushNum} name={"point"} text={"."} value={"."}></Button>
+            <Button input={pushEqual} name={"equal"} text={"="} value={"="}></Button>
+            <Button input={pushSymbol} name={"add"} text={"+"} value={"+"}></Button>
         </div>
     );
 }
