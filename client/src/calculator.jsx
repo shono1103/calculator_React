a6f40e91c7a53342f3b62b43c28b3f7ce8f733ff
shono131103 <shono131103@gmail.com>
2024-10-08 23:47:32 +0900

    #6 [fix] 小数点を一回押すと2回目は少数点を追加できないように変更

--- client/src/calculator.jsx
+++ client/src/calculator.jsx
@@ -6,14 +6,21 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 const Calculator = () => {
-    const [receivedData, setReceivedData] = useState("");
-    const [currentSymbol, setCurrentSymbol] = useState("");
-    const [result, setResult] = useState(0);
-    const [isFirst, setIsFirst] = useState(true);
-    const [display, setDisplay] = useState(0);
+    const [receivedData, setReceivedData] = useState("");   // 入力された値
+    const [currentSymbol, setCurrentSymbol] = useState("");   // 現在の演算子
+    const [result, setResult] = useState(0);    // 計算結果
+    const [isFirst, setIsFirst] = useState(true);   // 最初の数字かどうか
+    const [display, setDisplay] = useState("0");  // 表示する値
+    const [existPoint, setExistPoint] = useState(false);    // 小数点が存在するかどうか
 
     // 数字ボタンが押された時の処理
     const pushNumber = (value) => {
+        if (value === ".") {
+            if (existPoint) {
+                return;
+            }
+            setExistPoint(true);
+        }
         setReceivedData(receivedData + value);
         setDisplay(receivedData + value);
     }
@@ -28,19 +35,19 @@ const Calculator = () => {
         else {
             if (currentSymbol === "+") {
                 setResult(result + num);
-                setDisplay(result + num);
+                setDisplay(toString(result + num));
             }
             else if (currentSymbol === "-") {
                 setResult(result - num);
-                setDisplay(result - num);
+                setDisplay(toString(result - num));
             }
             else if (currentSymbol === "*") {
                 setResult(result * num);
-                setDisplay(result * num);
+                setDisplay(toString(result * num));
             }
             else if (currentSymbol === "/") {
                 setResult(result / num);
-                setDisplay(result / num);
+                setDisplay(toString(result / num));
             }
         }
         setCurrentSymbol(symbol);
@@ -53,19 +60,19 @@ const Calculator = () => {
         const num = parseFloat(receivedData);
         if (currentSymbol === "+") {
             setResult(result + num);
-            setDisplay(result + num);
+            setDisplay(toString(result + num));
         }
         else if (currentSymbol === "-") {
             setResult(result - num);
-            setDisplay(result - num);
+            setDisplay(toString(result - num));
         }
         else if (currentSymbol === "*") {
             setResult(result * num);
-            setDisplay(result * num);
+            setDisplay(toString(result * num));
         }
         else if (currentSymbol === "/") {
             setResult(result / num);
-            setDisplay(result / num);
+            setDisplay(toString(result / num));
         }
         setIsFirst(true);
         ClearAll();
@@ -74,6 +81,7 @@ const Calculator = () => {
     // Cボタンが押された時の処理
     const Clear = () => {
         setReceivedData("");
+        setExistPoint(false);
     }
 
     // CAボタンが押された時の処理
@@ -82,13 +90,17 @@ const Calculator = () => {
         setCurrentSymbol("");
         setResult(0);
         setIsFirst(true);
+        setExistPoint(false);
     }
 
+    // Cボタンが押された時の処理
     const pushClear = (non) => {
         Clear();
         setDisplay(0);
     }
 
+
+    // CAボタンが押された時の処理
     const pushClearAll = (non) => {
         ClearAll();
         setDisplay(0);
